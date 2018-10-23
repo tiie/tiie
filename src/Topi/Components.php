9a3269ff477ae70bed0203e5043d368552b94046
ttmdear <ttmdear@gmail.com>
2018-10-24 00:16:37 +0200

    [fix] Fix bug with component that loaded at second and next steps.

--- src/Topi/Components.php
+++ src/Topi/Components.php
@@ -27,6 +27,14 @@ class Components
         $this->config = $config;
     }
 
+    /**
+     * Returns the component with the given name.
+     *
+     * @param string $name Name of component.
+     * @param array $params Params are send to component during inicjalization.
+     * @param string $scope
+     * @return mixed
+     */
     public function get(string $name, array $params = array(), string $scope = null)
     {
         if ($name[0] == '@' && array_key_exists($name, $this->services)) {
@@ -57,29 +65,56 @@ class Components
         $this->scopes[$scope] = array();
         $component = $this->scopes[$scope][$name] = $this->initComponent($name, $params);
 
+        if ($name[0] == '@') {
+            if (!array_key_exists($name, $this->services)) {
+                $this->services[$name] = $component['object'];
+            }
+        }
+
         if (array_key_exists('after', $component['initer'])) {
             if (is_callable($component['initer']['after'])) {
                 call_user_func_array($component['initer']['after'], array($component['object'], new Scope($this, $scope), $params));
             }
         }
 
-        foreach ($this->scopes[$scope] as $componentname => $other) {
-            if ($componentname == $name) {
-                continue;
+        while(1) {
+            $ready = 1;
+
+            foreach ($this->scopes[$scope] as $key => $other) {
+                if ($key == $name) {
+                    continue;
+                }
+
+                if (!$other['ready']) {
+                    $ready = 0;
+                    break;
+                }
             }
 
-            if (!is_null($other['initer'])) {
-                if (array_key_exists('after', $other['initer'])) {
-                    if (is_callable($other['initer']['after'])) {
-                        call_user_func_array($other['initer']['after'], array($other['object'], new Scope($this, $scope), $params));
+            if ($ready) {
+                break;
+            }
+
+            foreach ($this->scopes[$scope] as $key => $other) {
+                if ($key == $name) {
+                    continue;
+                }
+
+                if (!is_null($other['initer'])) {
+                    if (array_key_exists('after', $other['initer'])) {
+                        if (is_callable($other['initer']['after'])) {
+                            call_user_func_array($other['initer']['after'], array($other['object'], new Scope($this, $scope), $params));
+                        }
                     }
                 }
-            }
 
-            if ($componentname[0] == '@') {
-                if (!array_key_exists($componentname, $this->services)) {
-                    $this->services[$componentname] = $other['object'];
-                }
+                $this->scopes[$scope][$key]['ready'] = 1;
+
+                // if ($key[0] == '@') {
+                //     if (!array_key_exists($key, $this->services)) {
+                //         $this->services[$key] = $other['object'];
+                //     }
+                // }
             }
         }
 
@@ -96,12 +131,14 @@ class Components
             return array(
                 'initer' => $initer,
                 'object' => $initer['init'],
+                'ready' => 0,
             );
         }
 
         return array(
             'initer' => $initer,
             'object' => call_user_func_array($initer['init'], array($this, $params)),
+            'ready' => 0,
         );
     }
 
